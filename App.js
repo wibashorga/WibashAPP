2e5b69aacaf4c6e2d17a27da0b317f2469445346
ElGranV <62523886+ElGranV@users.noreply.github.com>
2020-06-25 19:15:36 -0400

    Nouveau compo accueil

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Connexion from './components/page/connexion';
+import Accueil from './components/page/accueil';
 
 class Apps extends React.Component {
   render() {
     return (
-      <Connexion/>
+      <Accueil/>
     )
   }
 }

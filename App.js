d15ba0f5c1ef93da15779bfca23abc5387671fa6
blackol <anthonycolvil@gmail.com>
2020-06-24 22:42:47 -0400

    correction bug

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Connexion from './Connexion/Connexion';
-import { StyleSheet, Text, View } from 'react-native';
 
-eclass Apps extends React.Component {
+class Apps extends React.Component {
   render() {
     return (
       <Connexion/>

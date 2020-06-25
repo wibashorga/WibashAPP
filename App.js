26052754525dd3864348166ab0c150b4081b8eb7
blackol <anthonycolvil@gmail.com>
2020-06-24 22:37:11 -0400

    integration page de connection

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React from 'react';
+import Connexion from './Connexion/Connexion';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>J'ai tout réparer </Text>
-    </View>
-  );
+eclass Apps extends React.Component {
+  render() {
+    return (
+      <Connexion/>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Apps ;

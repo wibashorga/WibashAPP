78d33b4ed43f56288d903e11696695be4d5e5a45
blackol <anthonycolvil@gmail.com>
2020-07-14 22:36:05 -0400

    importation Navigation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Connexion from './components/page/connexion';
 import Accueil from './components/page/accueil';
+import Navigation from '.components/Navigation/Navigation';
 
 class Apps extends React.Component {
   render() {
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,1 +1,31 @@
-// Ne toucher pas ce dossier s'ils vous plaît
+// Components/Navigation/Navigation.js
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// vue principale 
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+// vue fille ou secondaire
+function DetailsScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }

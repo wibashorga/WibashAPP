cd07577023cb4dc870d738ffb44218ea0b20f373
blackol <anthonycolvil@gmail.com>
2020-07-19 05:45:22 -0400

    navigation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Connexion from './components/page/connexion';
 import Accueil from './components/page/accueil';
-import Navigation from '.components/Navigation/Navigation';
+import Navigation from './components/Navigation/Navigation';
 
 class Apps extends React.Component {
   render() {
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,31 +1,28 @@
-// Components/Navigation/Navigation.js
+// In App.js in a new project
+
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// vue principale 
-function HomeScreen({ navigation }) {
+function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
     </View>
   );
 }
 
-// vue fille ou secondaire
-function DetailsScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
+const Stack = createStackNavigator();
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigation;

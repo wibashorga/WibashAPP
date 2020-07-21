045cdfac4cd37251d2255bc6b3e3edea6188da1b
Chaps <62523886+ElGranV@users.noreply.github.com>
2020-07-21 17:23:45 -0400

    API de la BDD
    Les fonctions permettant de gérer les interactions avec la BDD seront stockées ici puis importées et intégrées au components

---
+++ components/api.js
@@ -0,0 +1,19 @@
+export async function createAccount(id, pass, pseudo, nom, prenom, phrase)
+{
+  let data = new FormData();
+  data.append("identifiant", id);
+  data.append("pass", pass);
+  data.append("pseudo", pseudo);
+  data.append("nom", nom);
+  data.append("prenom", prenom);
+  data.append("phrase", phrase);
+  fetch('http://www.wi-bash.fr/CreaCompte.php', {
+  method: 'POST',
+  headers: {
+    Accept: 'multipart/form-data',
+    'Content-Type': "multipart/form-data"
+  },
+  body: data
+}).then((reponse)=>reponse.text()).then((text)=>console.log(text)).catch((error)=>{console.log(error)});
+
+};

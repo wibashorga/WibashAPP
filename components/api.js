1e974a30a38bd59e04c4bd84ba3dd7945641728b
Chaps <62523886+ElGranV@users.noreply.github.com>
2020-07-22 13:37:44 -0400

    Update api.js

--- components/api.js
+++ components/api.js
@@ -1,3 +1,4 @@
+const token  = "PPlaFk63M4E6";
 function format(champ)
 {
   return champ.replace('"', '\"');
@@ -12,7 +13,7 @@ export async function createAccount(id, pass, pseudo, nom, prenom, mail, phrase)
   data.append("prenom", prenom);
   data.append("phrase", phrase);
   data.append("mail", mail);
-  data.append("token", "PPlaFk63M4E6");
+  data.append("token", token);
   fetch('http://www.wi-bash.fr/application/CreaCompte.php', {
   method: 'POST',
   headers: {
@@ -23,3 +24,21 @@ export async function createAccount(id, pass, pseudo, nom, prenom, mail, phrase)
 }).then((reponse)=>reponse.text()).then((text)=>console.log(text)).catch((error)=>{console.log(error)});
 
 };
+
+export async function login (identifiant, pass)
+{
+  let data = new FormData();
+  data.append("identifiant", id);
+  data.append("pass", pass);
+  data.append("token", token);
+  fetch('http://www.wi-bash.fr/application/CreaCompte.php', {
+  method: 'POST',
+  headers: {
+    Accept: 'multipart/form-data',
+    'Content-Type': "multipart/form-data"
+  },
+  body: data
+}).then((reponse)=>reponse.text()).then((text)=>JSON.parse(text)).catch((error)=>{console.log(error)});
+}
+
+

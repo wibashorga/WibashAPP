2b2a6a852006e072928b887d31dfe922a85cb776
blackol <anthonycolvil@gmail.com>
2020-08-04 18:04:11 -0400

    test

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Connexion from './components/page/connexion';
 import Accueil from './components/page/accueil';
 //import Navigation from './components/Navigation/Navigation';
-
+// test git
 class Apps extends React.Component {
   render() {
     return (

9b4ace3cad0d65cf826d85170afbb57f9a79edb0
blackol <anthonycolvil@gmail.com>
2020-08-04 21:31:10 -0400

    lol

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import Accueil from './components/page/acceuil';
+import Accueil from './components/page/accueil';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';

fae6d4cc1a218366e482f856d5497bd4c244fa06
blackol <anthonycolvil@gmail.com>
2020-08-04 21:25:08 -0400

    lol

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Connexion from './components/page/connexion';
-import Accueil from './components/page/accueil';
 import Navigation from './components/Navigation/Navigation';
 
 class Apps extends React.Component {
   render() {
     return (
-      <Accueil/>
+      <Navigation/>
     )
   }
 }
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,19 +1,15 @@
 // In App.js in a new project
 
 import * as React from 'react';
+import Accueil from './components/page/acceuil';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen({ navigation }) {
+const HomeScreen =({ navigation}) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
+   
+    <Accueil navigation={navigation}/>
   );
 }
 
@@ -21,12 +17,7 @@ function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      
     </View>
   );
 }

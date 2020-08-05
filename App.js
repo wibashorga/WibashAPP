9ac9506269cb083b0392702f9836f9b7a6f56fa3
blackol <anthonycolvil@gmail.com>
2020-08-04 20:38:46 -0400

    yolo

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Connexion from './components/page/connexion';
 import Accueil from './components/page/accueil';
-//import Navigation from './components/Navigation/Navigation';
-// test git
+import Navigation from './components/Navigation/Navigation';
+
 class Apps extends React.Component {
   render() {
     return (
-      <Acceui/>
+      <Accueil/>
     )
   }
 }

a012295dda9a6fe8e5b4fe9d34f7dec105cc445d
Chaps <62523886+ElGranV@users.noreply.github.com>
2020-08-07 17:26:31 -0400

    Update home.js

--- components/page/home.js
+++ components/page/home.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Modal, StyleSheet} from 'react-native';
 import Header from "./Header";
 
-export default class Home extends React.Component
-{
+const messages = ["Bon retour parmi nous, ", "Heureux de vous revoir, ",
+"Alors, motivé aujourd'hui ? ", "Wi-Bash n'était pas complet sans vous, "];
+
+export default class Home extends React.Component {
     constructor(props)
     {
         super(props);
         this.state = {
-           user : this.props.user
+           user : this.props.user,
+           bievenue : true
         }
-        
+        this.message = messages[parseInt(Math.random()*messages.length)];
+        setTimeout(()=> this.setState({bienvenue: false}), 2000);
     }
     render()
     {
         console.log("render")
         return(
             <View style = {{flex:1}}>
+                <Modal visible = {this.state.bienvenue} animationType = "slide"
+                transparent= {true}>
+                    <View style = {styles.modal}>
+                        <Text style= {styles.message}>{this.message+this.state.user.pseudo}</Text>
+                        </View>
+                </Modal>
+
                 <Header onPress = {()=>{}}/>
                 <Text>Bievenue, {this.state.user.pseudo} !</Text>
                 
@@ -24,3 +35,26 @@ export default class Home extends React.Component
         )
     }
 }
+
+const styles = StyleSheet.create(
+    {
+       modal:
+       {
+           backgroundColor: "rgb(156, 23, 84)",
+           alignSelf: "center",
+           marginTop: 100,
+           paddingVertical: 30,
+           paddingHorizontal: 5,
+           borderRadius: 20
+       },
+       message:
+       {
+           color: "white",
+           fontSize: 22,
+           fontStyle: "italic",
+           fontWeight: "bold"
+       }
+    }
+
+
+)

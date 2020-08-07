3ae0dabb470bb88eb86d501af26e40dd006ec60d
Chaps <62523886+ElGranV@users.noreply.github.com>
2020-08-07 14:21:03 -0400

    Create Header.js

---
+++ components/page/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, StyleSheet, StatusBar, Dimensions} from 'react-native';
+import {Icon} from "react-native-elements";
+
+const windowHeight = Dimensions.get("window").height;
+
+export default class Header extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+    render()
+    {
+        StatusBar.setHidden(true);
+        return(<View style = {styles.container}>
+            <Icon name = "menu" type = "Entypo" color = "white" iconStyle = {styles.icon}
+            onPress = {()=>this.props.onPress()}/>
+        </View>
+        )
+    }
+}
+const styles = StyleSheet.create(
+    {
+        container:
+        {
+            backgroundColor: "rgb(234,17,63)", 
+            flexDirection: "row",
+            alignContent: "center",
+            height: windowHeight/6,
+            
+        },
+        icon:
+        {
+            marginLeft: 10,
+            marginTop: 10,
+            alignSelf: "flex-start",
+        }
+    }
+)

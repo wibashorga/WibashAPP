ab988523f6de4ab78e9dfc4ee162c18671f08af4
Chaps <62523886+ElGranV@users.noreply.github.com>
2020-08-06 23:57:04 -0400

    la navigation gère le passage à l'accueil

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -2,41 +2,77 @@
 
 import * as React from 'react';
 import Accueil from '../page/accueil';
+import Home from "../page/home";
 import { Button, View, Text } from 'react-native';
 import Identification from '../page/identification';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-const HomeScreen =({ navigation}) => {
+const AccueilScreen =({ navigation}) => {
   return (
    
     <Accueil navigation={navigation}/>
   );
 }
 
-const IdentificationScreen = ({ route,navigation}) => {
-  return (
-    <Identification navigation={navigation, route}/>
-  );
-}
 
+const HomeScreen = ({route, navigation}) => {
+  return (
+    <Home navigation = {navigation, route}/>
+  )
+}
 
 
 
 const Stack = createStackNavigator();
 
-function Navigation() {
-  return (
-    <NavigationContainer>
-        
-      <Stack.Navigator initialRouteName="Home">
-
-      <Stack.Screen name="Home" component={HomeScreen} options={{title : "" , headerShown:false}} />
-        
-        <Stack.Screen name="identification" component={IdentificationScreen} options={{title: 'Identification',headerStyle: { backgroundColor: 'rgb(200,0,0)'},headerTintColor: '#fff' }} />
+class Navigation extends React.Component{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      connected: false,
+      profil: {}
+    }
+  }
+  sayConnected(profil)
+  {
+    this.setState({connected: true, utilisateur: profil});
+  }
+  authentification()
+  {
+    if (!this.state.connected){return(
+      <Stack.Navigator initialRouteName="Accueil">
+        <Stack.Screen name="Accueil" component={AccueilScreen} options={{title : "" , headerShown:false}} />
+          
+        <Stack.Screen name="identification"  options={{title: 'Identification',headerStyle: { backgroundColor: 'rgb(200,0,0)'},headerTintColor: '#fff' }}>
+        {props => <Identification {...props} sayConnected = {(profil)=> this.sayConnected(profil)} />}
+        </Stack.Screen>
       </Stack.Navigator>
+    )}else
+    {
+      return null;
+    }
+  }
+  homePage()
+  {
+    if (this.state.connected)
+    {return (
+      <Stack.Navigator initialRouteName = "Home">
+        <Stack.Screen name = "Home" component = {HomeScreen} 
+        initialParams = {{user : this.state.utilisateur}}/>
+      </Stack.Navigator>
+    )}
+  }
+
+  render()
+  {
+  return (
+    <NavigationContainer>  
+      {this.authentification()}
+      {this.homePage()}
     </NavigationContainer>
-  );
+  );}
 }
 
 export default Navigation;

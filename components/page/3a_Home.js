a44ff7d7f8a770c9cc9cfbc9478468d5f5099eb7
blackol <anthonycolvil@gmail.com>
2021-01-23 21:56:06 -0400

    fronted

--- components/page/3a_Home.js
+++ components/page/3a_Home.js
@@ -70,8 +70,14 @@ class DoubleCarteMembre extends React.Component
     {
      return(
         <View style={styles.CarteMembre}>
+          <Image source={require("./ressources/logo.png")} 
+                style= {{width:100, height:100, alignSelf:"center" , borderRadius:15}}></Image>
+            
         <Text style = {{fontWeight:"bold"}}>{membre.prenom+" "+membre.nom}</Text>
-        <Text>{membre.story} {membre.role}</Text>
+        <Text  style = {{fontWeight:"bold"}}>Role: </Text>
+        <Text> {membre.role}</Text>
+        <Text  style = {{fontWeight:"bold"}}>Histoire: </Text>
+        <Text > {membre.story}</Text>
     </View>
      )   
     }
@@ -87,6 +93,7 @@ class DoubleCarteMembre extends React.Component
             </View>
             
             <View style={styles.CarteMembre}>
+           
                 {this.memberDescritpion(this.membre2)}
             </View>
             </View>
@@ -289,40 +296,39 @@ export default class Home extends React.Component {
 
                         <View style = {styles.Titre}>
                             <Text style = {styles.textetitre} > Actu </Text>
+                            <Button title="Actu" onPress={()=>{this.setState({actuDialogVisible:true})}} /*Remplacer par un logo plus *//>
+
 
 
                     {this.state.actus.map(actu=>(<Text>{actu.actu}</Text>))}
+
+
                     <EditDialog visible={this.state.actuDialogVisible} inputCount={1}
-            firstInputHandler={(text)=>{this.actuContent=text}} close={()=>this.setState({actuDialogVisible:false})}
-            editButtonTitle="Editer l'actu" firstPlaceholder="Quelle bonne nouvelle allez-vous annoncer ?"
-            editAction={()=>{this.create_actu()
-                this.setState({actuDialogVisible:false})
-                this.actuContent=""}}/>
-            <Button title="Actu" onPress={()=>{this.setState({actuDialogVisible:true})}}/>
-
+                        firstInputHandler={(text)=>{this.actuContent=text}} close={()=>this.setState({actuDialogVisible:false})}
+                        editButtonTitle="Editer l'actu" firstPlaceholder="Quelle bonne nouvelle allez-vous annoncer ?"
+                         editAction={()=>{this.create_actu()
+                         this.setState({actuDialogVisible:false})
+                    this.actuContent=""}}/>
+            
 
 
                         </View>
 
-                        <View style = {styles.containtcarte}>
-                        
-
-                        </View>
                 </View>
 
 
 
-                <View style = {styles.categorie}>
+                <View style = {styles.categorie} /* modifier le style*/ >
 
                         <View style = {styles.Titre}>
                             <Text style = {styles.textetitre} > Les basheurs : </Text>
                             
                         </View>
 
-                        <View style = {{...styles.containtcarte, height:180}}>
+                        <View style = {{...styles.containtcarte, height:300}}>
                             <FlatList nestedScrollEnabled={true} data={setListAsPairs(this.state.membres)} keyExtractor={(item)=>
                             item[0].identifiant}
-                    renderItem= {(item)=><DoubleCarteMembre membres = {item.item}/>} />
+                    renderItem= {(item)=><DoubleCarteMembre membres = {item.item}/>}   horizontal = {true}/>
                        
 
                         </View>
@@ -425,9 +431,9 @@ const styles = StyleSheet.create(
        {
         
            width: 140,
-           height: 100,
+           height: 290,
            marginRight: 10,
-           marginTop:30,
+           marginTop:0,
            overflow: "hidden",
            paddingLeft:5,
           borderRadius: 20,
